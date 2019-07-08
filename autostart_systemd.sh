8462bb3184004000ee62487720c8be27131659ae
Bob Gray <bobgray1000@gmail.com>
2019-07-08 17:59:52 -0400

    Changed files to setup service

--- autostart_systemd.sh
+++ autostart_systemd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp analog_scanner@pi.service /etc/systemd/system/analog_scanner@${SUDO_USER:-${USER}}.service
-sed -i "s?/home/pi/scripts?`pwd`?" /etc/systemd/system/analog_scanner@${SUDO_USER:-${USER}}.service
+cp radio_manager@pi.service /etc/systemd/system/radio_manager@${SUDO_USER:-${USER}}.service
+sed -i "s?/home/pi/internet_radio?`pwd`?" /etc/systemd/system/radio_manager@${SUDO_USER:-${USER}}.service
 systemctl --system daemon-reload
 systemctl enable analog_scanner@${SUDO_USER:-${USER}}.service
