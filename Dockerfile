3729dd9457ce784542ff0595720b9030de7f5d5b
Bob Gray <bobgray1000@gmail.com>
2020-04-15 13:52:54 -0400

    Modified to use pudbus to detect speaker connection status

--- Dockerfile
+++ Dockerfile
@@ -11,15 +11,20 @@
 RUN apt-get update && apt-get install -y screen pianobar bluez-tools bluez bluetooth usbutils
 
 # Download InternetRadio app
-RUN git clone https://github.com/bg1000/InternetRadio.git
+# skip this for now and use local copy so we can make changes
+# RUN git clone https://github.com/bg1000/InternetRadio.git
 
 # RUN mkdir .config
 # RUN cd .config
 # RUN mkdir ./.config/pianobar
 # RUN cp * ./.config/pianobar/config
 
+# remove this when git clone is added back
+RUN mkdir .InternetRadio
+COPY requirements.txt ./InternetRadio/requirements.txt
+
 # Intall required python modules 
 RUN pip3 install --no-cache-dir -r ./InternetRadio/requirements.txt
 
 # Run GarageQTPi
-CMD ["/bin/sh", "./InternetRadio/startup.sh"]
+CMD ["/bin/sh", "/config/container_start.sh"]
--- radio_manager.py
+++ radio_manager.py
@@ -1,64 +1,52 @@
-#import os
-import subprocess
-import time
-import yaml
+# Standard library imports
 import os
+import subprocess
+import logging
+
+# Third party imports
+from gi.repository import GLib
+from pydbus import SystemBus
+import yaml
+
+# Local application imports
 import lib.utils
-import sys
-from subprocess import CalledProcessError
-if lib.utils.add_package_path("bluetool"):
-    from bluetool import Bluetooth
-else:
-    print("Can't find an installed version of bluetool. Exiting")
-    sys.exit(1)
-print ("Radio Manager Starting")
 
-#
-# Set up
-#
+# Callback then runs when the speakler connects/disconnects
+def on_state_change(sender, message, other):
+    # sender <-- string
+    # message <-- dictionary with properties that changed and values
+    # other <-- list (empty in this app)
+    if sender == 'org.bluez.Device1':
+        # check how many instance of radio app are running
+        radios.refresh() 
+        if message['Connected']:
+            logging.debug("Speaker connection detected.")
+            if radios.count == 0:
+                subprocess.call([radio_start_script])
+            elif radios.count >1:
+                radios.leaveOne
+        else:
+            logging.debug("Speaker disconnection detected.")
+            if radios.count > 0:
+                radios.killAll
 
-# allow this looping app to receive termination signals
-killer = lib.utils.GracefulKiller()
+# Read the config file, set up logging
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                       'radio_config.yaml'), 'r') as ymlfile:
+    CONFIG = yaml.safe_load(ymlfile)
+    logging.basicConfig(level=CONFIG["log_level"])
+    logging.info("Radio Manager has started.")
 # Setup Management of Pianobar
 radios = lib.utils.findProcByName("Pianobar")
-radio_start_script = str(os.path.join(os.path.abspath(os.path.dirname(__file__)))) + "/run_pianobar.sh"
-
-# open the config file
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'radio_config.yaml'), 'r') as ymlfile:
-    CONFIG = yaml.safe_load(ymlfile)
-
-bluetooth = Bluetooth()
-
-#
-# End Setup
-#
-
-#
-# Main Loop
-#
-if __name__ == "__main__":
-    while True:
-        if killer.kill_now:
-            break
-        print ("Checking to see if speaker is connected")
-
-        devices = bluetooth.get_connected_devices()
-        speaker_connected = False
-        if devices:
-            for device in devices:
-                if device["Address"] == CONFIG['speaker_address']:
-                    speaker_connected = True
-                    break
-        if (not speaker_connected and radios.count > 0):
-            radios.killAll()
-        elif (speaker_connected and radios.count == 0):
-            subprocess.call([radio_start_script])
-        elif (speaker_connected and radios.count > 1):
-            radios.leaveOne()
-        if killer.kill_now:
-          break
-
-        time.sleep(CONFIG['sleep_seconds']) 
-        radios.refresh() # refresh PID list for next iteration
+radio_start_script = CONFIG["radio_start_script"]
 
 
+# Subscribe to dbus signal and start loop
+bus = SystemBus()
+speaker = bus.get('org.bluez',
+                  '/org/bluez/hci0/dev_' + 
+                  CONFIG["speaker_address"].replace(":", "_"))
+
+speaker.onPropertiesChanged = on_state_change
+loop = GLib.MainLoop()
+loop.run() # blocks until callback comes in
