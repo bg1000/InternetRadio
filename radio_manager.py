b9eab1e944646422b7adeed6d0a62143d116469d
Bob Gray <bobgray1000@gmail.com>
2020-04-15 14:22:59 -0400

    autopep8

--- radio_manager.py
+++ radio_manager.py
@@ -12,23 +12,26 @@ import yaml
 import lib.utils
 
 # Callback then runs when the speakler connects/disconnects
+
+
 def on_state_change(sender, message, other):
     # sender <-- string
     # message <-- dictionary with properties that changed and values
     # other <-- list (empty in this app)
     if sender == 'org.bluez.Device1':
         # check how many instance of radio app are running
-        radios.refresh() 
+        radios.refresh()
         if message['Connected']:
             logging.debug("Speaker connection detected.")
             if radios.count == 0:
                 subprocess.call([radio_start_script])
-            elif radios.count >1:
+            elif radios.count > 1:
                 radios.leaveOne
         else:
             logging.debug("Speaker disconnection detected.")
             if radios.count > 0:
                 radios.killAll
+
 
 # Read the config file, set up logging
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
@@ -44,9 +47,9 @@ radio_start_script = CONFIG["radio_start_script"]
 # Subscribe to dbus signal and start loop
 bus = SystemBus()
 speaker = bus.get('org.bluez',
-                  '/org/bluez/hci0/dev_' + 
+                  '/org/bluez/hci0/dev_' +
                   CONFIG["speaker_address"].replace(":", "_"))
 
 speaker.onPropertiesChanged = on_state_change
 loop = GLib.MainLoop()
-loop.run() # blocks until callback comes in
+loop.run()  # blocks until callback comes in
