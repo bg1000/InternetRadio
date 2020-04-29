3114654b2d76cfda540b3db56c09b7d6b072467e
Bob Gray <bobgray1000@gmail.com>
2020-04-29 15:29:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-mkdir InternetRadio
-cd InternetRadio
 git clone https://github.com/bg1000/InternetRadio
+cd InternetRadio
 python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
